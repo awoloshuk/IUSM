9fffa8a7e9314b935010e5c5f51cd251a29bbd18
Andre Woloshuk <awoloshuk@gmail.com>
2019-05-21 16:07:30 +0000

    added functionality for german traffic sign dataset

--- MNIST/data_loader/data_loaders.py
+++ MNIST/data_loader/data_loaders.py
@@ -46,3 +46,18 @@ class cifar10_DataLoader(BaseDataLoader):
         importlib.reload(databases) #used to get load any recent changes from database class
         self.dataset = databases.Cifar10Dataset('data/CIFAR10/cifar-10-batches-py/data_batch_1' ,transforms = trsfm)
         super(cifar10_DataLoader, self).__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
+        
+        
+class traffic_DataLoader(BaseDataLoader):
+    def __init__(self, csv_path, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True):
+        trsfm = transforms.Compose([
+            transforms.RandomRotation(20),
+            transforms.Resize(32),
+            transforms.CenterCrop(32),
+            transforms.ToTensor(),
+        ])
+        self.data_dir = data_dir
+        self.csv_path = csv_path
+        importlib.reload(databases) #used to get load any recent changes from database class
+        self.dataset = databases.trafficDataset(csv_path, data_dir, transforms=trsfm)
+        super(traffic_DataLoader, self).__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
--- MNIST/data_loader/databases.py
+++ MNIST/data_loader/databases.py
@@ -9,6 +9,7 @@ from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
 from PIL import Image
 
+
 class Cifar10Dataset(Dataset):
     def unpickle(self, file):
         import pickle
@@ -67,5 +68,26 @@ class MnistCSVDataset(Dataset):
     def __len__(self):
         return self.data_len
 
+
+class trafficDataset(Dataset):
+    def __init__(self, csv_path, root_dir, transforms=None):
+        self.csv_data = pd.read_csv(csv_path, header = 0)
+        self.root_dir = root_dir
+        self.transforms = transforms
+        self.data_len = len(self.csv_data)
+        
+    def __getitem__(self, index):
+        img_name = os.path.join(self.root_dir,
+                                self.csv_data.iloc[index, 7]) #path data is in the 7th column
+        image = io.imread(img_name)
+        img_as_img = Image.fromarray(image) #convert to PIL
+        label = self.csv_data.iloc[index, 6]  #label is in the 6th column
+        if self.transforms is not None:
+            img_as_tensor = self.transforms(img_as_img)
+        return img_as_tensor, label
+        
+    
+    def __len__(self):
+        return self.data_len
 
     
